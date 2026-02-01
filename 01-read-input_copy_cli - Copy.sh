5a48af3155032890b0382b613da9ec734953dbe4
Keyvaun <keyvaunroblox@gmail.com>
2026-02-01 15:55:53 -0500

    Modified the code

---
+++ 01-read-input_copy_cli - Copy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo " hello Keyvaun!"
+echo "How old are you?" 
+read 23
+echo 'Thanks Keyvaun ! I see you are ' $age ' years old'
+
+echo "What school do you currently attend?"
+read GGC
+echo 'Nice! ' $school ' sounds like a great place to learn.'
